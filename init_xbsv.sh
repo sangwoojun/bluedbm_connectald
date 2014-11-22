2110fa923439cf2e42e60980652b1f322ba65d51
Ming Liu <mingliu@mit.edu>
2014-11-22 16:30:48 -0500

    recorded working connectal version

--- init_xbsv.sh
+++ init_xbsv.sh
@@ -3,6 +3,9 @@
 mkdir -p tools
 cd tools;
 git clone https://github.com/cambridgehackers/connectal.git xbsv
+#connectal version: 696f3faa5ea3cf1073fe3f0b38a530df331ca796
+#working on flash @ 1.2GB/s
 
 git clone https://github.com/cambridgehackers/fpgamake.git
 git clone https://github.com/cambridgehackers/buildcache.git
+
